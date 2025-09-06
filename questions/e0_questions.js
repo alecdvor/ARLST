bc938506003a7bff52d3d028258946920d64a15f
Alec <56179545+alecdvor@users.noreply.github.com>
2025-09-06 00:17:45 -0400

    Create e0_questions.js

---
+++ questions/e0_questions.js
@@ -0,0 +1,136 @@
+var e0_questions = {
+    "questions": [
+        {
+            "id": "E0A01",
+            "question": "What is the primary function of an external earth connection or ground rod?",
+            "options": [
+                "A. Prevent static build up on power lines",
+                "B. Lightning charge dissipation",
+                "C. Reduce RF current flow between pieces of equipment",
+                "D. Protect breaker panel from power surges"
+            ],
+            "answer": "B. Lightning charge dissipation"
+        },
+        {
+            "id": "E0A02",
+            "question": "When evaluating RF exposure levels from your station at a neighbor’s home, what must you do?",
+            "options": [
+                "A. Ensure signals from your station are less than the controlled maximum permissible exposure (MPE) limits",
+                "B. Ensure signals from your station are less than the uncontrolled maximum permissible exposure (MPE) limits",
+                "C. Ensure signals from your station are less than the controlled maximum permissible emission (MPE) limits",
+                "D. Ensure signals from your station are less than the uncontrolled maximum permissible emission (MPE) limits"
+            ],
+            "answer": "B. Ensure signals from your station are less than the uncontrolled maximum permissible exposure (MPE) limits"
+        },
+        {
+            "id": "E0A03",
+            "question": "Over what range of frequencies are the FCC human body RF exposure limits most restrictive?",
+            "options": [
+                "A. 300 kHz - 3 MHz",
+                "B. 3 - 30 MHz",
+                "C. 30 - 300 MHz",
+                "D. 300 - 3000 MHz"
+            ],
+            "answer": "C. 30 - 300 MHz"
+        },
+        {
+            "id": "E0A04",
+            "question": "When evaluating a site with multiple transmitters operating at the same time, the operators and licensees of which transmitters are responsible for mitigating over-exposure situations?",
+            "options": [
+                "A. Each transmitter that produces 20 percent or more of its MPE limit in areas where the total MPE limit is exceeded",
+                "B. Each transmitter operating with a duty cycle greater than 25 percent",
+                "C. Each transmitter that produces 5 percent or more of its MPE limit in areas where the total MPE limit is exceeded",
+                "D. Each transmitter operating with a duty cycle greater than 50 percent"
+            ],
+            "answer": "C. Each transmitter that produces 5 percent or more of its MPE limit in areas where the total MPE limit is exceeded"
+        },
+        {
+            "id": "E0A05",
+            "question": "What hazard is created by operating at microwave frequencies?",
+            "options": [
+                "A. Microwaves are ionizing radiation",
+                "B. The high gain antennas commonly used can result in high exposure levels",
+                "C. Microwaves are in the frequency range where wave velocity is higher",
+                "D. The extremely high frequency energy can damage the joints of antenna structures"
+            ],
+            "answer": "B. The high gain antennas commonly used can result in high exposure levels"
+        },
+        {
+            "id": "E0A06",
+            "question": "Why are there separate electric (E) and magnetic (H) MPE limits at frequencies below 300 MHz?",
+            "options": [
+                "A. The body reacts to electromagnetic radiation from both the E and H fields",
+                "B. Ground reflections and scattering cause the field strength to vary with location",
+                "C. E field and H field radiation intensity peaks can occur at different locations",
+                "D. All these choices are correct"
+            ],
+            "answer": "D. All these choices are correct"
+        },
+        {
+            "id": "E0A07",
+            "question": "What is meant by “100% tie-off” regarding tower safety?",
+            "options": [
+                "A. All loose ropes and guys secured to a fixed structure",
+                "B. At least one lanyard attached to the tower at all times",
+                "C. All tools secured to the climber’s harness",
+                "D. All circuit breakers feeding power to the tower must be tied closed with tape, cable, or ties"
+            ],
+            "answer": "B. At least one lanyard attached to the tower at all times"
+        },
+        {
+            "id": "E0A08",
+            "question": "What does SAR measure?",
+            "options": [
+                "A. Signal attenuation ratio",
+                "B. Signal amplification rating",
+                "C. The rate at which RF energy is absorbed by the body",
+                "D. The rate of RF energy reflected from stationary terrain"
+            ],
+            "answer": "C. The rate at which RF energy is absorbed by the body"
+        },
+        {
+            "id": "E0A09",
+            "question": "Which of the following types of equipment are exempt from RF exposure evaluations?",
+            "options": [
+                "A. Transceivers with less than 7 watts of RF output",
+                "B. Antennas that radiate only in the near field",
+                "C. Hand-held transceivers sold before May 3, 2021",
+                "D. Dish antennas less than one meter in diameter"
+            ],
+            "answer": "C. Hand-held transceivers sold before May 3, 2021"
+        },
+        {
+            "id": "E0A10",
+            "question": "When must an RF exposure evaluation be performed on an amateur station operating on 80 meters?",
+            "options": [
+                "A. An evaluation must always be performed",
+                "B. When the ERP of the station is less than 10 watts",
+                "C. When the station’s operating mode is CW",
+                "D. When the output power from the transmitter is less than 100 watts"
+            ],
+            "answer": "A. An evaluation must always be performed"
+        },
+        {
+            "id": "E0A11",
+            "question": "To what should lanyards be attached while climbing?",
+            "options": [
+                "A. Antenna mast",
+                "B. Guy brackets",
+                "C. Tower rungs",
+                "D. Tower legs"
+            ],
+            "answer": "D. Tower legs"
+        },
+        {
+            "id": "E0A12",
+            "question": "Where should a shock-absorbing lanyard be attached to a tower when working above ground?",
+            "options": [
+                "A. Above the climber’s head level",
+                "B. To the belt of the fall-arrest harness",
+                "C. Even with the climber's waist",
+                "D. To the next lowest set of guys"
+            ],
+            "answer": "A. Above the climber’s head level"
+        }
+    ]
+}
