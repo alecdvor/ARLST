2255a4c65b6c831058f1ec240869ef8289c477ed
Alec <56179545+alecdvor@users.noreply.github.com>
2025-09-05 23:16:19 -0400

    Create t6_questions.js

---
+++ questions/t6_questions.js
@@ -0,0 +1,533 @@
+var t6_questions = {
+    "questions": [
+        {
+            "id": "T6A01",
+            "question": "What electrical component opposes the flow of current in a DC circuit?",
+            "options": [
+                "A. Inductor",
+                "B. Resistor",
+                "C. Inverter",
+                "D. Transformer"
+            ],
+            "answer": "B. Resistor"
+        },
+        {
+            "id": "T6A02",
+            "question": "What type of component is often used as an adjustable volume control?",
+            "options": [
+                "A. Fixed resistor",
+                "B. Power resistor",
+                "C. Potentiometer",
+                "D. Transformer"
+            ],
+            "answer": "C. Potentiometer"
+        },
+        {
+            "id": "T6A03",
+            "question": "What electrical parameter is controlled by a potentiometer?",
+            "options": [
+                "A. Inductance",
+                "B. Resistance",
+                "C. Capacitance",
+                "D. Field strength"
+            ],
+            "answer": "B. Resistance"
+        },
+        {
+            "id": "T6A04",
+            "question": "What electrical component stores energy in an electric field?",
+            "options": [
+                "A. Varistor",
+                "B. Capacitor",
+                "C. Inductor",
+                "D. Diode"
+            ],
+            "answer": "B. Capacitor"
+        },
+        {
+            "id": "T6A05",
+            "question": "What type of electrical component consists of conductive surfaces separated by an insulator?",
+            "options": [
+                "A. Resistor",
+                "B. Potentiometer",
+                "C. Oscillator",
+                "D. Capacitor"
+            ],
+            "answer": "D. Capacitor"
+        },
+        {
+            "id": "T6A06",
+            "question": "What type of electrical component stores energy in a magnetic field?",
+            "options": [
+                "A. Varistor",
+                "B. Capacitor",
+                "C. Inductor",
+                "D. Diode"
+            ],
+            "answer": "C. Inductor"
+        },
+        {
+            "id": "T6A07",
+            "question": "What electrical component is typically constructed as a coil of wire?",
+            "options": [
+                "A. Switch",
+                "B. Capacitor",
+                "C. Diode",
+                "D. Inductor"
+            ],
+            "answer": "D. Inductor"
+        },
+        {
+            "id": "T6A08",
+            "question": "What is the function of an SPDT switch?",
+            "options": [
+                "A. A single circuit is opened or closed",
+                "B. Two circuits are opened or closed",
+                "C. A single circuit is switched between one of two other circuits",
+                "D. Two circuits are each switched between one of two other circuits"
+            ],
+            "answer": "C. A single circuit is switched between one of two other circuits"
+        },
+        {
+            "id": "T6A09",
+            "question": "What electrical component is used to protect other circuit components from current overloads?",
+            "options": [
+                "A. Fuse",
+                "B. Thyratron",
+                "C. Varactor",
+                "D. All these choices are correct"
+            ],
+            "answer": "A. Fuse"
+        },
+        {
+            "id": "T6A10",
+            "question": "Which of the following battery chemistries is rechargeable?",
+            "options": [
+                "A. Nickel-metal hydride",
+                "B. Lithium-ion",
+                "C. Lead-acid",
+                "D. All these choices are correct"
+            ],
+            "answer": "D. All these choices are correct"
+        },
+        {
+            "id": "T6A11",
+            "question": "Which of the following battery chemistries is not rechargeable?",
+            "options": [
+                "A. Nickel-cadmium",
+                "B. Carbon-zinc",
+                "C. Lead-acid",
+                "D. Lithium-ion"
+            ],
+            "answer": "B. Carbon-zinc"
+        },
+        {
+            "id": "T6A12",
+            "question": "What type of switch is represented by component 3 in figure T-2?",
+            "options": [
+                "A. Single-pole single-throw",
+                "B. Single-pole double-throw",
+                "C. Double-pole single-throw",
+                "D. Double-pole double-throw"
+            ],
+            "answer": "A. Single-pole single-throw",
+            "image": "figureT-2.png"
+        },
+        {
+            "id": "T6B01",
+            "question": "Which is true about forward voltage drop in a diode?",
+            "options": [
+                "A. It is lower in some diode types than in others",
+                "B. It is proportional to peak inverse voltage",
+                "C. It indicates that the diode is defective",
+                "D. It has no impact on the voltage delivered to the load"
+            ],
+            "answer": "A. It is lower in some diode types than in others"
+        },
+        {
+            "id": "T6B02",
+            "question": "What electronic component allows current to flow in only one direction?",
+            "options": [
+                "A. Resistor",
+                "B. Fuse",
+                "C. Diode",
+                "D. Driven element"
+            ],
+            "answer": "C. Diode"
+        },
+        {
+            "id": "T6B03",
+            "question": "Which of these components can be used as an electronic switch?",
+            "options": [
+                "A. Varistor",
+                "B. Potentiometer",
+                "C. Transistor",
+                "D. Thermistor"
+            ],
+            "answer": "C. Transistor"
+        },
+        {
+            "id": "T6B04",
+            "question": "Which of the following components can consist of three regions of semiconductor material?",
+            "options": [
+                "A. Alternator",
+                "B. Transistor",
+                "C. Triode",
+                "D. Pentagrid converter"
+            ],
+            "answer": "B. Transistor"
+        },
+        {
+            "id": "T6B05",
+            "question": "What type of transistor has a gate, drain, and source?",
+            "options": [
+                "A. Varistor",
+                "B. Field-effect",
+                "C. Tesla-effect",
+                "D. Bipolar junction"
+            ],
+            "answer": "B. Field-effect"
+        },
+        {
+            "id": "T6B06",
+            "question": "How is the cathode lead of a semiconductor diode often marked on the package?",
+            "options": [
+                "A. With the word \"cathode\"",
+                "B. With a stripe",
+                "C. With the letter C",
+                "D. With the letter K"
+            ],
+            "answer": "B. With a stripe"
+        },
+        {
+            "id": "T6B07",
+            "question": "What causes a light-emitting diode (LED) to emit light?",
+            "options": [
+                "A. Forward current",
+                "B. Reverse current",
+                "C. Capacitively-coupled RF signal",
+                "D. Inductively-coupled RF signal"
+            ],
+            "answer": "A. Forward current"
+        },
+        {
+            "id": "T6B08",
+            "question": "What does the abbreviation FET stand for?",
+            "options": [
+                "A. Frequency Emission Transmitter",
+                "B. Fast Electron Transistor",
+                "C. Free Electron Transmitter",
+                "D. Field Effect Transistor"
+            ],
+            "answer": "D. Field Effect Transistor"
+        },
+        {
+            "id": "T6B09",
+            "question": "What are the names for the electrodes of a diode?",
+            "options": [
+                "A. Plus and minus",
+                "B. Source and drain",
+                "C. Anode and cathode",
+                "D. Gate and base"
+            ],
+            "answer": "C. Anode and cathode"
+        },
+        {
+            "id": "T6B10",
+            "question": "Which of the following can provide power gain?",
+            "options": [
+                "A. Transformer",
+                "B. Transistor",
+                "C. Reactor",
+                "D. Resistor"
+            ],
+            "answer": "B. Transistor"
+        },
+        {
+            "id": "T6B11",
+            "question": "What is the term that describes a device's ability to amplify a signal?",
+            "options": [
+                "A. Gain",
+                "B. Forward resistance",
+                "C. Forward voltage drop",
+                "D. On resistance"
+            ],
+            "answer": "A. Gain"
+        },
+        {
+            "id": "T6B12",
+            "question": "What are the names of the electrodes of a bipolar junction transistor?",
+            "options": [
+                "A. Signal, bias, power",
+                "B. Emitter, base, collector",
+                "C. Input, output, supply",
+                "D. Pole one, pole two, output"
+            ],
+            "answer": "B. Emitter, base, collector"
+        },
+        {
+            "id": "T6C01",
+            "question": "What is the name of an electrical wiring diagram that uses standard component symbols?",
+            "options": [
+                "A. Bill of materials",
+                "B. Connector pinout",
+                "C. Schematic",
+                "D. Flow chart"
+            ],
+            "answer": "C. Schematic"
+        },
+        {
+            "id": "T6C02",
+            "question": "What is component 1 in figure T-1?",
+            "options": [
+                "A. Resistor",
+                "B. Transistor",
+                "C. Battery",
+                "D. Connector"
+            ],
+            "answer": "A. Resistor",
+            "image": "figureT-1.png"
+        },
+        {
+            "id": "T6C03",
+            "question": "What is component 2 in figure T-1?",
+            "options": [
+                "A. Resistor",
+                "B. Transistor",
+                "C. Indicator lamp",
+                "D. Connector"
+            ],
+            "answer": "B. Transistor",
+            "image": "figureT-1.png"
+        },
+        {
+            "id": "T6C04",
+            "question": "What is component 3 in figure T-1?",
+            "options": [
+                "A. Resistor",
+                "B. Transistor",
+                "C. Lamp",
+                "D. Ground symbol"
+            ],
+            "answer": "C. Lamp",
+            "image": "figureT-1.png"
+        },
+        {
+            "id": "T6C05",
+            "question": "What is component 4 in figure T-1?",
+            "options": [
+                "A. Resistor",
+                "B. Transistor",
+                "C. Ground symbol",
+                "D. Battery"
+            ],
+            "answer": "D. Battery",
+            "image": "figureT-1.png"
+        },
+        {
+            "id": "T6C06",
+            "question": "What is component 6 in figure T-2?",
+            "options": [
+                "A. Resistor",
+                "B. Capacitor",
+                "C. Regulator IC",
+                "D. Transistor"
+            ],
+            "answer": "B. Capacitor",
+            "image": "figureT-2.png"
+        },
+        {
+            "id": "T6C07",
+            "question": "What is component 8 in figure T-2?",
+            "options": [
+                "A. Resistor",
+                "B. Inductor",
+                "C. Regulator IC",
+                "D. Light emitting diode"
+            ],
+            "answer": "D. Light emitting diode",
+            "image": "figureT-2.png"
+        },
+        {
+            "id": "T6C08",
+            "question": "What is component 9 in figure T-2?",
+            "options": [
+                "A. Variable capacitor",
+                "B. Variable inductor",
+                "C. Variable resistor",
+                "D. Variable transformer"
+            ],
+            "answer": "C. Variable resistor",
+            "image": "figureT-2.png"
+        },
+        {
+            "id": "T6C09",
+            "question": "What is component 4 in figure T-2?",
+            "options": [
+                "A. Variable inductor",
+                "B. Double-pole switch",
+                "C. Potentiometer",
+                "D. Transformer"
+            ],
+            "answer": "D. Transformer",
+            "image": "figureT-2.png"
+        },
+        {
+            "id": "T6C10",
+            "question": "What is component 3 in figure T-3?",
+            "options": [
+                "A. Connector",
+                "B. Meter",
+                "C. Variable capacitor",
+                "D. Variable inductor"
+            ],
+            "answer": "D. Variable inductor",
+            "image": "figureT-3.png"
+        },
+        {
+            "id": "T6C11",
+            "question": "What is component 4 in figure T-3?",
+            "options": [
+                "A. Antenna",
+                "B. Transmitter",
+                "C. Dummy load",
+                "D. Ground"
+            ],
+            "answer": "A. Antenna",
+            "image": "figureT-3.png"
+        },
+        {
+            "id": "T6C12",
+            "question": "Which of the following is accurately represented in electrical schematics?",
+            "options": [
+                "A. Wire lengths",
+                "B. Physical appearance of components",
+                "C. Component connections",
+                "D. All these choices are correct"
+            ],
+            "answer": "C. Component connections"
+        },
+        {
+            "id": "T6D01",
+            "question": "Which of the following devices or circuits changes an alternating current into a varying direct current signal?",
+            "options": [
+                "A. Transformer",
+                "B. Rectifier",
+                "C. Amplifier",
+                "D. Reflector"
+            ],
+            "answer": "B. Rectifier"
+        },
+        {
+            "id": "T6D02",
+            "question": "What is a relay?",
+            "options": [
+                "A. An electrically-controlled switch",
+                "B. A current controlled amplifier",
+                "C. An inverting amplifier",
+                "D. A pass transistor"
+            ],
+            "answer": "A. An electrically-controlled switch"
+        },
+        {
+            "id": "T6D03",
+            "question": "Which of the following is a reason to use shielded wire?",
+            "options": [
+                "A. To decrease the resistance of DC power connections",
+                "B. To increase the current carrying capability of the wire",
+                "C. To prevent coupling of unwanted signals to or from the wire",
+                "D. To couple the wire to other signals"
+            ],
+            "answer": "C. To prevent coupling of unwanted signals to or from the wire"
+        },
+        {
+            "id": "T6D04",
+            "question": "Which of the following displays an electrical quantity as a numeric value?",
+            "options": [
+                "A. Potentiometer",
+                "B. Transistor",
+                "C. Meter",
+                "D. Relay"
+            ],
+            "answer": "C. Meter"
+        },
+        {
+            "id": "T6D05",
+            "question": "What type of circuit controls the amount of voltage from a power supply?",
+            "options": [
+                "A. Regulator",
+                "B. Oscillator",
+                "C. Filter",
+                "D. Phase inverter"
+            ],
+            "answer": "A. Regulator"
+        },
+        {
+            "id": "T6D06",
+            "question": "What component changes 120 V AC power to a lower AC voltage for other uses?",
+            "options": [
+                "A. Variable capacitor",
+                "B. Transformer",
+                "C. Transistor",
+                "D. Diode"
+            ],
+            "answer": "B. Transformer"
+        },
+        {
+            "id": "T6D07",
+            "question": "Which of the following is commonly used as a visual indicator?",
+            "options": [
+                "A. LED",
+                "B. FET",
+                "C. Zener diode",
+                "D. Bipolar transistor"
+            ],
+            "answer": "A. LED"
+        },
+        {
+            "id": "T6D08",
+            "question": "Which of the following is combined with an inductor to make a resonant circuit?",
+            "options": [
+                "A. Resistor",
+                "B. Zener diode",
+                "C. Potentiometer",
+                "D. Capacitor"
+            ],
+            "answer": "D. Capacitor"
+        },
+        {
+            "id": "T6D09",
+            "question": "What is the name of a device that combines several semiconductors and other components into one package?",
+            "options": [
+                "A. Transducer",
+                "B. Multi-pole relay",
+                "C. Integrated circuit",
+                "D. Transformer"
+            ],
+            "answer": "C. Integrated circuit"
+        },
+        {
+            "id": "T6D10",
+            "question": "What is the function of component 2 in figure T-1?",
+            "options": [
+                "A. Give off light when current flows through it",
+                "B. Supply electrical energy",
+                "C. Control the flow of current",
+                "D. Convert electrical energy into radio waves"
+            ],
+            "answer": "C. Control the flow of current",
+            "image": "figureT-1.png"
+        },
+        {
+            "id": "T6D11",
+            "question": "Which of the following is a resonant or tuned circuit?",
+            "options": [
+                "A. An inductor and a capacitor in series or parallel",
+                "B. A linear voltage regulator",
+                "C. A resistor circuit used for reducing standing wave ratio",
+                "D. A circuit designed to provide high-fidelity audio"
+            ],
+            "answer": "A. An inductor and a capacitor in series or parallel"
+        }
+    ]
+}
